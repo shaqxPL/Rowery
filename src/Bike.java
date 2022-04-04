89ffc052d3866c251da9f1f13755f74bba6efdb0
shaqxPL <tomasz198720@gmail.com>
2022-04-04 19:53:03 +0200

    Dziedziczenie - test

--- src/Bike.java
+++ src/Bike.java
@@ -1,41 +1,41 @@
-public class Bike {
-
-    private int gear;
-    private int speed;
-
-    public Bike(int startGear, int startSpeed) {
-        this.gear = startGear;
-        this.speed = startSpeed;
-    }
-
-
-    public int getGear() {
-        return gear;
-    }
-
-    public void setGear(int gear) {
-        this.gear = gear;
-    }
-
-    public int getSpeed() {
-        return speed;
-    }
-
-    public void setSpeed(int speed) {
-        this.speed = speed;
-    }
-
-    public void applyBrake(int decrement) {
-        this.speed = speed - decrement;
-    }
-
-    public void speedUp(int increment) {
-        this.speed += increment;
-//        this.speed = speed + increment;     skrocony zapis tego jest w linii 33.
-    }
-
-    @Override
-    public String toString() {
-        return "Bike: gear - " + this.gear + ", speed - " + this.speed;
-    }
-}
+//public class Bike {
+//
+//    private int gear;
+//    private int speed;
+//
+//    public Bike(int startGear, int startSpeed) {
+//        this.gear = startGear;
+//        this.speed = startSpeed;
+//    }
+//
+//
+//    public int getGear() {
+//        return gear;
+//    }
+//
+//    public void setGear(int gear) {
+//        this.gear = gear;
+//    }
+//
+//    public int getSpeed() {
+//        return speed;
+//    }
+//
+//    public void setSpeed(int speed) {
+//        this.speed = speed;
+//    }
+//
+//    public void applyBrake(int decrement) {
+//        this.speed = speed - decrement;
+//    }
+//
+//    public void speedUp(int increment) {
+//        this.speed += increment;
+////        this.speed = speed + increment;     skrocony zapis tego jest w linii 33.
+//    }
+//
+//    @Override
+//    public String toString() {
+//        return "Bike: gear - " + this.gear + ", speed - " + this.speed;
+//    }
+//}
--- src/MountainBike.java
+++ src/MountainBike.java
@@ -1,21 +1,21 @@
-public class MountainBike extends Bike {
-
-    private int amountAmortizator;
-
-    public MountainBike(int startGear, int startSpeed, int amountAmortizator) {
-        super(startGear, startSpeed);
-        this.amountAmortizator = amountAmortizator;
-    }
-
-        public void setAmountAmortizator(int amountAmortizator ) {
-            this.amountAmortizator = amountAmortizator;
-        }
-
-    @Override
-    public String toString() {
-        return super.toString() + ", Number of Amortizators - " + this.amountAmortizator;
-    }
-}
+//public class MountainBike extends Bike {
+//
+//    private int amountAmortizator;
+//
+//    public MountainBike(int startGear, int startSpeed, int amountAmortizator) {
+//        super(startGear, startSpeed);
+//        this.amountAmortizator = amountAmortizator;
+//    }
+//
+//        public void setAmountAmortizator(int amountAmortizator ) {
+//            this.amountAmortizator = amountAmortizator;
+//        }
+//
+//    @Override
+//    public String toString() {
+//        return super.toString() + ", Number of Amortizators - " + this.amountAmortizator;
+//    }
+//}
 
 
 
